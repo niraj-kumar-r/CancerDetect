e5ce7b3c4f374f38c7b8fd9023d02dcad31b845c
Shafi Khan <khanshafi1905@gmail.com>
2023-08-23 14:52:32 +0530

    updated Disclaimer

--- app/src/main/java/com/example/trial2/IntroActivity.kt
+++ app/src/main/java/com/example/trial2/IntroActivity.kt
@@ -1,24 +1,40 @@
-// IntroActivity.kt
 package com.example.trial2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trial2.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntroBinding
+    private var isDisclaimerAccepted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnNextIntro.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent) // Move to the main activity
-            //finish() // Close this intro activity...
+        binding.checkBoxAgree.setOnCheckedChangeListener { _, isChecked ->
+            isDisclaimerAccepted = isChecked
+            updateNextButtonColor()
         }
+
+        binding.btnNextIntro.setOnClickListener {
+            if (isDisclaimerAccepted) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent) // Move to the main activity
+                finish() // Close this intro activity
+            } else {
+                // Show a warning message
+                Toast.makeText(this, "Please accept the disclaimer before proceeding.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateNextButtonColor() {
+        val colorResId = if (isDisclaimerAccepted) R.color.safetyorange else R.color.orangered
+        binding.btnNextIntro.setBackgroundColor(resources.getColor(colorResId, theme))
     }
 }
